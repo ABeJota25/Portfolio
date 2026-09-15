3d645c5ce55a5f09b1cf22565f149b092b04c0db
AaronBJ25 <aaronbj86@alumnos.cei.es>
2026-09-15 19:51:28 +0200

    feat: Adición de primer proyecto

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const SCROLL_DOWN_ARROW = document.getElementById('scrollDownArrow');
+const PROJECTS_SECTION = document.getElementById('projectsSection');
 
 SCROLL_DOWN_ARROW.addEventListener('click', () => {
-    
+    PROJECTS_SECTION.scrollIntoView()
 })
